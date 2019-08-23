002f8f3a084bdbabb0bc735362fbce6dfa3d7f33
Ishu27 <36397472+Ishu27@users.noreply.github.com>
2019-08-23 14:47:32 +0530

    Update demo4.py

--- actions/demo4.py
+++ actions/demo4.py
@@ -3,9 +3,17 @@ import requests
 from st2common.runners.base_action import Action
 
 class abc(Action):
-	response = request.get('https://google.com')
-	def run(self, response):
-		if response.status_code == 200:
-        		print('Success!')
-		elif response.status_code == 404:
-    			print('Not Found.')
+	
+	def run(self, url):
+		
+		try:
+			response = requests.get(url)
+			print(response.status_code)
+			print(response.url)
+			
+		except requests.exception.MissingSchema:
+			print("Wrong URL")
+			sys.exit(0)
+			
+			
+			
